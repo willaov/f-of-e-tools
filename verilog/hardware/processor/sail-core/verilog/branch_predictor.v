1ab02a4109b735d1a06ee21ad271ec236c36d763
Philip Sosnin <philipsosnin@gmail.com>
2022-05-27 07:49:27 +0100

    global correlation bp

--- verilog/hardware/processor/sail-core/verilog/branch_predictor.v
+++ verilog/hardware/processor/sail-core/verilog/branch_predictor.v
@@ -55,24 +55,41 @@ module branch_predictor(
 	 *	inputs
 	 */
 	input		clk;
+
+	// outcome from last predicted instruction
 	input		actual_branch_decision;
+
+	// 1 if instruction currently in IF stage is a branch
 	input		branch_decode_sig;
+
+	// 1 if instruction currently in MA stage is a branch
 	input		branch_mem_sig;
+
+	// Pc of the instruction currently in IF stage
 	input [31:0]	in_addr;
+
+	// Imm offset of the branch instruction currently in IF stage
 	input [31:0]	offset;
+
+	// Global history shift register
+	reg [5:0]	history_shift_reg;
+
+	// Pattern history table
+	reg [1:0]	pattern_history_table[0:63];
 
 	/*
 	 *	outputs
 	 */
+	// PC address of the current pc + branch offset
 	output [31:0]	branch_addr;
+	
+	// 1 if we predict a branch for the instruction currently in IF stage
 	output		prediction;
 
-	/*
-	 *	internal state
-	 */
-	reg [1:0]	s;
-
 	reg		branch_mem_sig_reg;
+	
+	// state indexed by pattern_history_table
+	reg [1:0]	s;
 
 	/*
 	 *	The `initial` statement below uses Yosys's support for nonzero
@@ -85,10 +102,11 @@ module branch_predictor(
 	 *	modules in the design and to thereby set the values.
 	 */
 	initial begin
-		s = 2'b00;
+		history_shift_reg = 6'b0;
 		branch_mem_sig_reg = 1'b0;
 	end
 
+	// store if instruction currently in MA stage is a branch
 	always @(negedge clk) begin
 		branch_mem_sig_reg <= branch_mem_sig;
 	end
@@ -98,13 +116,19 @@ module branch_predictor(
 	 *	therefore can use branch_mem_sig as every branch is followed by
 	 *	a bubble, so a 0 to 1 transition
 	 */
+	// update the FSM based on the outcome of the prediction of the branch currently in 
 	always @(posedge clk) begin
+		s <= pattern_history_table[history_shift_reg];
 		if (branch_mem_sig_reg) begin
 			s[1] <= (s[1]&s[0]) | (s[0]&actual_branch_decision) | (s[1]&actual_branch_decision);
 			s[0] <= (s[1]&(!s[0])) | ((!s[0])&actual_branch_decision) | (s[1]&actual_branch_decision);
+			pattern_history_table[history_shift_reg] <= s;
+			history_shift_reg <= history_shift_reg << 1 + actual_branch_decision;
 		end
 	end
 
+	// address of next instruction if branch taken
 	assign branch_addr = in_addr + offset;
+	// only predict a branch if in state 10 or 11 and the instruction in IF is a branch
 	assign prediction = s[1] & branch_decode_sig;
 endmodule
