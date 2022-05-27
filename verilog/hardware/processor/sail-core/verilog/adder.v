5ece92e38015c1fc929c9c397a1cda3284f005ee
William Vinnicombe <william.vinnicombe@gmail.com>
2022-05-27 08:50:26 +0000

    DSP Adder now implemented

--- verilog/hardware/processor/sail-core/verilog/adder.v
+++ verilog/hardware/processor/sail-core/verilog/adder.v
@@ -50,11 +50,11 @@ module adder(input1, input2, out);
 	input [31:0]	input2;
 	output [31:0]	out;
 
-	// hard_adder internal(
-	// 	.input1(input1),
-	// 	.input2(input2),
-	// 	.out(out)
-	// );
+	hard_adder internal(
+		.input1(input1),
+		.input2(input2),
+		.out(out)
+	);
 
 	// lookahead_32bit_adder internal(
 	// 	.a(input1),
@@ -68,7 +68,7 @@ module adder(input1, input2, out);
 	// 	.out(out)
 	// );
 
-	assign		out = input1 + input2;
+	// assign		out = input1 + input2;
 endmodule
 
 /* 	This carry_lookahead_adder_4_bit module is downloaded
@@ -346,53 +346,35 @@ module hard_adder(input1, input2, out);
 	input [31:0]	input2;
 	output [31:0]	out;
 
-	// Things it needs
-	reg clk = 1'b1;	// Doesn't actually need a clock as all unregistered
-	reg ce = 1'b0;
-	reg irsttop = 1'b0;
-	reg irstbot = 1'b0;
-	reg osrttop = 1'b0;
-	reg orstbot = 1'b0;
-	reg ahold = 1'b0;
-	reg bhold = 1'b0;
-	reg chold = 1'b0;
-	reg dhold = 1'b0;
-	reg oholdtop = 1'b0;
-	reg oholdbot = 1'b0;
-	reg oloadtop = 1'b0;
-	reg oloadbot = 1'b0;
-	reg addsubtop = 1'b0;
-	reg addsubbot = 1'b0;
-	reg ci = 1'b0;
-	// reg co = 1'b0;
-	reg signextin = 1'b0;
-	// reg signextout = 1'b0;
-
-
 	SB_MAC16 i_sbmac16(
 		.A(input1[31:16]),
 		.B(input1[15:0]),
 		.C(input2[31:16]),
 		.D(input2[15:0]),
-		.O(out),
-		.CLK(clk),
-		.CE(ce),
-		.IRSTTOP(irsttop),
-		.IRSTBOT(irstbot),
-		.ORSTTOP(osrttop),
-		.ORSTBOT(orstbot),
-		.AHOLD(ahold),
-		.BHOLD(bhold),
-		.CHOLD(chold),
-		.DHOLD(dhold),
-		.OHOLDTOP(oholdtop),
-		.OHOLDBOT(oholdbot),
-		.OLOADTOP(oloadtop),
-		.OLOADBOT(oloadbot),
-		.ADDSUBTOP(addsubtop),
-		.ADDSUBBOT(addsubbot),
-		.CI(ci),
-		.SIGNEXTIN(signextin)
+		.O(out[31:0]),
+		.CLK(), // no clocked elements connected
+		.CE(1'b0), //no clock, clock input disable 
+		.IRSTTOP(1'b0),
+		.IRSTBOT(1'b0),
+		.ORSTTOP(1'b0),
+		.ORSTBOT(1'b0),
+		.AHOLD(1'b0),
+		.BHOLD(1'b0),
+		.CHOLD(1'b0),
+		.DHOLD(1'b0),
+		.OHOLDTOP(1'b0),
+		.OHOLDBOT(1'b0),
+		.OLOADTOP(1'b0),
+		.OLOADBOT(1'b0),
+		.ADDSUBTOP(1'b0),
+		.ADDSUBBOT(1'b0),
+		.CO(),
+		.CI(1'b0),
+ 		//MAC cascading ports.
+		.ACCUMCI(1'b0),
+		.ACCUMCO(),
+		.SIGNEXTIN(1'b0),
+		.SIGNEXTOUT()
 	);
 
 	defparam i_sbmac16.NEG_TRIGGER = 1'b0;
@@ -409,12 +391,12 @@ module hard_adder(input1, input2, out);
 	defparam i_sbmac16.TOPOUTPUT_SELECT = 2'b00;
 	defparam i_sbmac16.TOPADDSUB_LOWERINPUT = 2'b00;
 	defparam i_sbmac16.TOPADDSUB_UPPERINPUT = 1'b1;
-	defparam i_sbmac16.TOPADDSUB_CARRYSELECT = 2'b11;
+	defparam i_sbmac16.TOPADDSUB_CARRYSELECT = 2'b10;
 	defparam i_sbmac16.BOTOUTPUT_SELECT = 2'b00;
 	defparam i_sbmac16.BOTADDSUB_LOWERINPUT = 2'b00;
 	defparam i_sbmac16.BOTADDSUB_UPPERINPUT = 1'b1;
 	defparam i_sbmac16.BOTADDSUB_CARRYSELECT = 2'b00;
-	defparam i_sbmac16.MODE_8x8 = 1'b0;
+	defparam i_sbmac16.MODE_8x8 = 1'b1;
 	defparam i_sbmac16.A_SIGNED = 1'b0;
 	defparam i_sbmac16.B_SIGNED = 1'b0;
 endmodule
